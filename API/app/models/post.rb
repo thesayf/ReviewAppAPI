3c5ba43dff594413764a4ca9edf2087817cbbefa
ladiadeniran <oladipupo.adeniran@gmail.com>
2020-01-24 06:21:23 +0100

    implement the indexing on the post model

--- API/app/models/post.rb
+++ API/app/models/post.rb
@@ -1,74 +1,52 @@
 class Post < ApplicationRecord
-  include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
+  include Searchable
+
   belongs_to :user
   has_many :comments
   has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
 
-  mapping do
-    byebug
-    indexes :title
-    indexes :description
-    indexes :address
-    indexes :video
-    indexes :longitude, type: 'integer'
-    indexes :latitude, type: 'integer'
-    indexes :user_id, type: 'integer'
-    indexes "tags", type: "nested" do
-            indexes "name", type: "text"
-          end 
+
+  after_save    :re_index
+  after_destroy :delete_index
+
+  mappings dynamic: 'false' do
+    indexes :title, type: :keyword
+    indexes :description, type: :keyword
+    indexes :address, type: :keyword
+    indexes :video, type: :keyword
+    indexes :longitude, type: :integer
+    indexes :latitude, type: :integer
+    indexes :user_id, type: :integer
+
+    indexes :taggings, type: :nested
+    indexes :tags, type: :nested do
+      indexes :name, type: :text, analyzer: :english
+    end
   end
 
+  def as_indexed_json(options = {})
+    self.as_json(
+      options.merge(
+        only: [:title, :description, :address, :video, :longitude, :latitude, :user_id],
+        include: { tags: { only: [:name] } }
+      )
+    )
+  end
 
-  # indexes :tags, type: 'nested' do
-  #   indexes :name
-  # end
+  # params type tag: string
+  def self.search_by_tags(tag, query = ::Query::Posts::ByTags.new)
+    self.search(query.call(tag)).records.to_a
+  end
 
-  # def self.search_posts_by_tags(query)
-  #   self.search({
-  #     query: {
-  #       bool: {
-  #         must: [
-  #         {
-  #           multi_match: {
-  #             query: query,
-  #             fields: [:author, :title, :body, :tags]
-  #           }
-  #         },
-  #         {
-  #           match: {
-  #             published: true
-  #           }
-  #         }]
-  #       }
-  #     }
-  #   })
-  # end
-
-   # settings do
-  #   mappings do
-  #     indexes :title, type: :text
-  #     indexes :description, type: :text, analyzer: :english
-  #     indexes :longitude, type: :integer
-  #     indexes :latitiude, type: :integer
-  #     indexes :address, type: :text, analyzer: :english
-  #     indexes :user_id, type: :integer
-  #     indexes :video, type: :text, analyzer: :english
-  #     indexes :tags, type: :nested do
-  #       indexes :name, type: :text
-  #     end 
-  #   end 
-
-  # end
+  # params type tag: string, long: integer, lat; integer
+  def self.search_by_tags_and_location(tag:, long:, lat:, query: ::Query::Posts::ByTagsAndLocation.new)
+    self.search(query.call(tag, long, lat)).records.to_a
+  end
 
   def all_tags=(names)
     self.tags = names.map do |name|
-        Tag.where(name: name).first_or_create!
+      Tag.where(name: name).first_or_create!
+    end
   end
-
-  end
-
-
-  
 end
