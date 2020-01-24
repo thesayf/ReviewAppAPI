7a8cfa9767cdf2edcd50205e635822c197871ef7
ladiadeniran <oladipupo.adeniran@gmail.com>
2020-01-24 06:22:24 +0100

    Add the searchable module to the model related to Post

--- API/app/models/tagging.rb
+++ API/app/models/tagging.rb
@@ -1,6 +1,19 @@
 class Tagging < ApplicationRecord
-  include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
+  include Searchable
+
   belongs_to :tag
   belongs_to :post
+
+  after_save :post_index
+  after_destroy :delete_post_index
+
+  private
+
+  def post_index
+    post.re_index
+  end
+
+  def delete_post_index
+    post.delete_index
+  end
 end
